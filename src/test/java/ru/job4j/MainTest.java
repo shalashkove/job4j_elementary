5708bb06123dd5f38e945b16fde11ecd4df6c04d
shalashkove <shalashkove@gmail.com>
2020-06-07 18:29:41 +0500

    2. Добавление Maven в существующий проект.[274137#297495]

--- src/test/java/ru/job4j/MainTest.java
+++ src/test/java/ru/job4j/MainTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-//...
+
 public class MainTest {
     @Test
     public void whenActionThenResult() {
